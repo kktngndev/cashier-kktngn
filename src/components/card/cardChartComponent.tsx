11c0092386b329b3acdc727de4c66bcbb6cc5632
Muhammad Arief <azkklk@gmail.com>
2023-10-10 22:38:53 +0800

    chore(chart): fix chartjs import register

--- src/components/card/cardChartComponent.tsx
+++ src/components/card/cardChartComponent.tsx
@@ -1,31 +1,16 @@
 'use client'
 import React from 'react'
-import {
-  Chart as ChartJS, CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js'
-import { Chart } from 'react-chartjs-2'
+import Chart from 'chart.js/auto'
+import { Bar } from 'react-chartjs-2'
 
-ChartJS.register(CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend)
+Chart.register()
 
 export default function CardChartComponent() {
   return (
     <div className='w-[750px] h-fit bg-hacienda-100 font-semibold rounded-xl p-5'>
       <h1 className='text-xl text-hacienda-900'>Penjualan per bulan</h1>
       <div className='h-72 w-full rounded-xl mt-5 flex justify-center items-center'>
-        <Chart
-          type='bar'
+        <Bar
           data={{
             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun'],
             datasets: [
