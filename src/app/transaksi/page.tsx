40a80e5deff47b0d8cbe87be90ef8893a1363321
Muhammad Arief <azkklk@gmail.com>
2023-10-23 22:04:50 +0800

    chore(transaksi): remove scrollbar

--- src/app/transaksi/page.tsx
+++ src/app/transaksi/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
     <div className='flex flex-1'>
       <Navbar />
       <div className='w-screen grid grid-cols-12'>
-        <div className='h-screen overflow-auto col-span-8 p-6'>
+        <div className='h-screen overflow-auto col-span-8 p-6 scrollbar-hide'>
           <HeaderTrx />
           <div className='p-2 mt-4 grid grid-cols-3 justify-items-center gap-6'>
             <CardMenuComponent />
