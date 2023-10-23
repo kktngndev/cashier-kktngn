fbe5e4bfca589a05137fe368d5da6a03d2e580b2
Muhammad Arief <azkklk@gmail.com>
2023-10-23 15:52:33 +0800

    feat(transaksi): add card menu component

--- src/app/transaksi/page.tsx
+++ src/app/transaksi/page.tsx
@@ -1,13 +1,23 @@
 import Navbar from '@/components/navbar/navbar'
 import HeaderTrx from '@/components/headerTrxComponent'
+import CardMenuComponent from '@/components/card/cardMenuComponent'
 
 export default function Page() {
   return (
     <div className='flex flex-1'>
       <Navbar />
       <div className='w-screen grid grid-cols-12'>
-        <div className='h-screen col-span-8 p-6'>
+        <div className='h-screen overflow-auto col-span-8 p-6'>
           <HeaderTrx />
+          <div className='p-2 mt-4 grid grid-cols-3 justify-items-center gap-6'>
+            <CardMenuComponent />
+            <CardMenuComponent />
+            <CardMenuComponent />
+            <CardMenuComponent />
+            <CardMenuComponent />
+            <CardMenuComponent />
+            <CardMenuComponent />
+          </div>
         </div>
         <div className='h-screen col-span-4 bg-hacienda-200 shadow-lg'>
           2
