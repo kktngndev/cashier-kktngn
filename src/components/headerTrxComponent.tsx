c47e23c2fd07f6af1a283cc7c3cf352e1847c965
Muhammad Arief <azkklk@gmail.com>
2023-10-23 15:51:46 +0800

    chore(label): add isDefault parameter

--- src/components/headerTrxComponent.tsx
+++ src/components/headerTrxComponent.tsx
@@ -3,11 +3,10 @@ import CardLabelComponent from '@/components/card/cardLabelComponent'
 
 export default async function HeaderTrx(){
   const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getKategoriData`).then(res => res.json())
-  console.log(data);
-  
+
   return(
     <div className='h-16 w-full flex gap-7 items-center'>
-      <CardLabelComponent name='Semua' />
+      <CardLabelComponent name='Semua' isDefault />
       {
         data.map((item: { nama_kategori: string }, index: Key | null | undefined) => (
           <CardLabelComponent key={ index } name={ item.nama_kategori } />
