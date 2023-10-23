faff258d5563fc36a9015a867bc575cdfda707b6
Muhammad Arief <azkklk@gmail.com>
2023-10-23 21:22:15 +0800

    feat(main): add recoil context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import './globals.css'
 import { Montserrat } from 'next/font/google'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
+import RecoilContextProvider from '../utils/recoilContextProvider'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <body className={`${montserrat.className}`}>
         <ProgressBar color="#6f5814" height='4px' shallowRouting options={{ showSpinner: false }} />
-        {children}
+        <RecoilContextProvider>
+          {children}
+        </RecoilContextProvider>
       </body>
     </html>
   )
