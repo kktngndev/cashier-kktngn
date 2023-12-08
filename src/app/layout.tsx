f30cbd26698a4774ea67c80ffa4ba6bae6a5a1f1
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-08 19:33:26 +0800

    feat(transaksi): handling filter list menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Montserrat } from 'next/font/google'
 import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import RecoilContextProvider from '../utils/recoilContextProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -10,14 +11,18 @@ type Props = {
   children: React.ReactNode
 }
 
+const queryClient = new QueryClient()
+
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={`${montserrat.className}`}>
         <ProgressBar color="#6f5814" height='4px' shallowRouting options={{ showSpinner: false }} />
-        <RecoilContextProvider>
-          {children}
-        </RecoilContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <RecoilContextProvider>
+            {children}
+          </RecoilContextProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
--- src/components/list/listCardCategoriesTrxComponent.tsx
+++ src/components/list/listCardCategoriesTrxComponent.tsx
@@ -1,28 +1,46 @@
 'use client'
 import React from 'react'
 import CardMenuComponent from '../card/cardMenuComponent'
+import { useQuery } from '@tanstack/react-query'
+import { useRecoilState } from 'recoil'
+import { checkedCategoryAtom } from '../../../atoms/categoryAtom'
 
 export default function ListCardCategoriesTrxComponent() {
-  const [menu, setMenu] = React.useState([])
-
-  React.useEffect(() => {
-
-    async function getData(){
-      const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProdukData`).then(res => res.json())
-      setMenu(data)
-      console.log(data);
+  const [checkedCategory, setCheckedCategory] = useRecoilState(checkedCategoryAtom)
+  
+  const getData = async () => {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getProdukData`)
+    const res = await data.json()
+    return {
+      data: res,
+      isKopi: res.filter((item: any) => item.kategori.nama_kategori === 'Kopi'),
+      isNonKopi: res.filter((item: any) => item.kategori.nama_kategori === 'Non Kopi'),
     }
-    getData()
-  }, [])
+  }
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['produk'],
+    queryFn: getData,
+    refetchInterval: 1000
+  })
+
+  const isCoffee = data?.isKopi?.map((item: { nama_produk: string, harga_produk: number, id: number }, index: number) => (
+    <CardMenuComponent key={index} name={item.nama_produk} price={item.harga_produk} />
+  ))
+
+  const isNonCoffee = data?.isNonKopi?.map((item: { nama_produk: string, harga_produk: number, id: number }, index: number) => (
+    <CardMenuComponent key={index} name={item.nama_produk} price={item.harga_produk} />
+  ))
 
   return (
     <div className='p-2 mt-4 grid grid-cols-3 justify-items-center gap-6'>
       {
-        menu ? (
-          menu.map((item: { nama_produk: string, harga_produk: number, id: number }, index) => (
-            <CardMenuComponent key={ index } name={ item.nama_produk } price={ item.harga_produk } />
-          ))
-        ) : null
+        isLoading && <div>Loading...</div>
+      }
+      {
+        data && checkedCategory === 'Kopi' ? isCoffee : checkedCategory === 'Non Kopi' ? isNonCoffee : data?.data.map((item: { nama_produk: string, harga_produk: number, id: number }, index: number) => (
+          <CardMenuComponent key={index} name={item.nama_produk} price={item.harga_produk} />
+        ))
       }
     </div>
   )
