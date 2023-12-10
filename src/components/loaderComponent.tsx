a175e079b9f8cf619a45519019642f4fd185fc2f
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-10 17:35:27 +0800

    chore: refactor loader

--- src/components/loaderComponent.tsx
+++ src/components/loaderComponent.tsx
@@ -2,7 +2,7 @@ import { BarLoader } from "react-spinners";
 
 export default function LoaderComponent() {
   return (
-    <div className='flex h-screen justify-center items-center'>
+    <div className='flex h-screen w-screen justify-center items-center absolute'>
       <BarLoader color='#6f5814' className="text-3xl" />
     </div>
   )
