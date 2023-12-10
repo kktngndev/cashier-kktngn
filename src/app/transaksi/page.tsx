868b6f1b0c8f4cb31c8bb266fdf97021145666bb
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-10 17:35:15 +0800

    feat(transaction): add cart and method transaction

--- src/app/transaksi/page.tsx
+++ src/app/transaksi/page.tsx
@@ -1,38 +1,71 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import Navbar from '@/components/navbar/navbar'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { checkedCategoryAtom } from '../../../atoms/categoryAtom'
+import { cartItemAtom, cartTotalAtom } from '../../../atoms/cartAtom'
 import ListCardCategoriesTrxComponent from '@/components/list/listCardCategoriesTrxComponent'
 import CardLabelComponent from '@/components/card/cardLabelComponent'
+import CardCartComponent from '@/components/card/cardCartComponent'
 
 export default function Page() {
   const [checkedCategory, setCheckedCategory] = useRecoilState(checkedCategoryAtom)
   const [categoryData, setCategoryData] = useState([])
+  const [cartItem, setCartItem] = useRecoilState(cartItemAtom) as any[]
+  const total = useRecoilValue(cartTotalAtom)
+
   useEffect(() => {
-    async function getCategoryData(){
+    async function getCategoryData() {
       const getData = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/getKategoriData`).then(res => res.json())
       setCategoryData(getData)
     }
     getCategoryData()
-  },[checkedCategory])
+  }, [checkedCategory])
+
   return (
     <div className='flex flex-1'>
       <Navbar />
       <div className='w-screen grid grid-cols-12'>
         <div className='h-screen overflow-auto col-span-9 p-6 scrollbar-hide'>
           <div className='h-16 w-full flex gap-7 items-center'>
-            <CardLabelComponent name='Semua' onChange={(e) => setCheckedCategory(e.target.checked)}/>
             {
               categoryData && categoryData.map((item: { nama_kategori: string }, index: number) => (
-                <CardLabelComponent key={ index } name={ item.nama_kategori } onChange={(e) => setCheckedCategory(e.target.value)} />
+                <CardLabelComponent key={index} name={item.nama_kategori} onChange={(e) => setCheckedCategory(e.target.value)} />
               ))
             }
           </div>
           <ListCardCategoriesTrxComponent />
         </div>
-        <div className='h-screen col-span-3 bg-hacienda-200 shadow-lg'>
-          2
+        <div className='h-screen overflow-auto scrollbar-hide col-span-3 bg-hacienda-200 shadow-lg p-3 w-full mx-auto'>
+          <div className='border-b-2 p-2 mx-4 border-hacienda-950'>
+            <h1 className='text-2xl font-bold'>Keranjang</h1>
+          </div>
+          <div className='mt-7 px-3'>
+            {
+              cartItem.length > 0 ? cartItem.map((item: any, index: number) => (
+                <CardCartComponent key={index} item={item} />
+              ))
+              : <p className='text-center text-xl font-semibold'>Keranjang Kosong</p>
+            }
+          </div>
+          <div className='relative mt-6 p-3 bottom-0 flex flex-col justify-between gap-6 h-[350px] border-t-2 border-hacienda-950'>
+            <div className='flex items-start justify-between'>
+              <h1 className='font-bold text-3xl'>Total: </h1>
+              <p className='text-3xl'>Rp. {total}</p>
+            </div>
+            <div>
+              <h4 className='font-semibold text-xl text-center'>Metode Pembayaran</h4>
+              <div className='flex gap-8 mt-4 justify-center'>
+                <input type='radio' className='metodeBayarRadio' name='Bayar' id='Tunai' value='tunai' />
+                <label className='metodeBayarLabel' htmlFor='Tunai'>Tunai</label>
+                <input type='radio' className='metodeBayarRadio' name='Bayar' id='Non Tunai' value='nontunai' />
+                <label className='metodeBayarLabel' htmlFor='Non Tunai'>Non Tunai</label>
+              </div>
+            </div>
+            <div className='w-full flex justify-center mt-5'>
+              <button className='w-full h-16 bg-hacienda-700 rounded-xl border transition border-hacienda-950 text-white font-semibold hover:bg-hacienda-600 hover:text-hacienda-900'>Proses</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
