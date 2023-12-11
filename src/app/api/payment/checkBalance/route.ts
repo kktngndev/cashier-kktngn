8928fcf925c451e6f19042fc5ba3cb4cd3ba7d07
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-11 16:16:18 +0800

    refactor: refactor code to convert env string to base64

--- src/app/api/payment/checkBalance/route.ts
+++ src/app/api/payment/checkBalance/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(){
   const balance = await fetch(`${process.env.NEXT_PUBLIC_FLIP_API_URL}/general/balance`,{
     headers: {
-      'Authorization': `basic ${process.env.NEXT_PUBLIC_FLIP_API_SECRET} `
+      'Authorization': `basic ${btoa(`${process.env.NEXT_PUBLIC_FLIP_API_SECRET?.toString()}`)}`
     }
   }).then(res => res.json())
 
