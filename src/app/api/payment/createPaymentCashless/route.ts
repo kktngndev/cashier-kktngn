a3f04b848ed15fc25af2c2b377d6ae3141f433e8
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-12 19:47:26 +0800

    refactor(env): change variable environment name

--- src/app/api/payment/createPaymentCashless/route.ts
+++ src/app/api/payment/createPaymentCashless/route.ts
@@ -3,9 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest){
   const { amount, title } = await req.json();
 
-  const createQRIS = await fetch(`${process.env.NEXT_PUBLIC_FLIP_API_URL}/pwf/bill`, {
+  const createQRIS = await fetch(`${process.env.FLIP_API_URL}/pwf/bill`, {
     headers: {
-      'Authorization' : `basic ${btoa(`${process.env.NEXT_PUBLIC_FLIP_API_SECRET?.toString()}`)}`,
+      'Authorization' : `basic ${btoa(`${process.env.FLIP_API_SECRET?.toString()}`)}`,
       'Content-Type' : 'application/json',
       
     },
