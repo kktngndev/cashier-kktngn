86bce8fcb9e26ec395823c26ec07569b67ee5a32
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-19 10:44:57 +0800

    feat(payment): send transaction report to database

--- src/app/api/payment/callback/route.ts
+++ src/app/api/payment/callback/route.ts
@@ -1,21 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "@/utils/supabase";
 
 export async function POST(req: NextRequest) {
   try {
     const data = await req.formData();
     const res = JSON.parse(`${data.getAll('data')}`);
+    console.log(res);
 
     if (res.status === 'SUCCESSFUL') {
+      const { data: transaksi, error } = await supabase.from('transaksi').insert([
+        
+      ])
       console.log('Payment Success');
       return NextResponse.json({ message: 'Payment Success' });
     }else if(res.status === 'PENDING'){
       console.log('Payment Pending');
       return NextResponse.json({ message: 'Payment Pending' });
+    }else {
+      throw new Error('Payment Failed');
     }
-    console.log('Payment Failed');
-    return NextResponse.json({ message: 'Payment Failed' }, { status: 500 });
-  } catch (e) {
-    console.error(e);
+  } catch (e: any) {
+    console.error(e.message);
     return NextResponse.json({ message: 'Error' }, { status: 500 });
   }
 }
