27b19ce804059b622eb14857aee8ac04daaf5433
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-20 15:00:59 +0800

    feat(api): add data when callback triggered

--- src/app/api/payment/callback/route.ts
+++ src/app/api/payment/callback/route.ts
@@ -4,15 +4,27 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.formData();
     const res = JSON.parse(`${data.getAll('data')}`);
-    console.log(res);
 
     if (res.status === 'SUCCESSFUL') {
+      const params = {
+        id: res.sender_name,
+        status_transaksi: res.status,
+        metode_pembayaran: res.sender_bank,
+        total_pembayaran: res.amount,
+      }
+      const addData = await fetch(`https://85sx9k0n-3000.asse.devtunnels.ms/api/transaction/create`, {
+        method: 'POST',
+        body: JSON.stringify(params),
+      }).then(res => res.json());
+      if (addData) {
+        console.log('Data Added');
+      }
       console.log('Payment Success');
       return NextResponse.json({ message: 'Payment Success' });
-    }else if(res.status === 'PENDING'){
+    } else if (res.status === 'PENDING') {
       console.log('Payment Pending');
       return NextResponse.json({ message: 'Payment Pending' });
-    }else {
+    } else {
       throw new Error('Payment Failed');
     }
   } catch (e: any) {
