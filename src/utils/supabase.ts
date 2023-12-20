2e7814b289a25c94be35bd63d0fcc07a97c59f3f
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-20 15:02:30 +0800

    chore: remove comment

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,5 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '../../types/database'
 
-// export const supabase = createClient<Database>(`${process.env.NEXT_PUBLIC_SUPABASE_URL}`,`${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`)
 export const supabase = createClientComponentClient<Database>({ supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL, supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY})
 
