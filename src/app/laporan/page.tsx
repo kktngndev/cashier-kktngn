abc697fb8b4085c51fa0a2ed53edf9b5caa84c5a
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-22 11:23:47 +0800

    feat(laporan): add laporan page

--- src/app/laporan/page.tsx
+++ src/app/laporan/page.tsx
@@ -1,18 +1,72 @@
 'use client'
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo, ReactNode } from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { Navbar } from '@/components/navbar'
-import DataTable from 'react-data-table-component'
+import { LoaderComponent, Navbar } from '@/components'
+import DataTable, { TableColumn } from 'react-data-table-component'
+import moment from 'moment'
+import 'moment/locale/id'
+
+type DataRow = {
+  id_transaksi: string,
+  metode_pembayaran: string,
+  status_transaksi:  JSX.Element,
+  total_pembayaran: number,
+  created_at: string
+}
 
 export default function Page() {
-  const columns = useMemo(() => [], [])
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['transaksi'],
+    queryFn: async () => {
+      const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/transaction`)
+      return data.json()
+    },
+  })
+
+  const columns: TableColumn<DataRow>[] = useMemo(() => [
+    {
+      name: 'ID Transaksi',
+      sortable: true,
+      selector: (row: { id_transaksi: any }) => row.id_transaksi,
+    },
+    {
+      name: 'Metode Pembayaran',
+      sortable: true,
+      selector: (row: { metode_pembayaran: any }) => String(row.metode_pembayaran).toUpperCase()
+    },
+    {
+      name: 'Status',
+      sortable: true,
+      cell: (row: { status_transaksi: any }) => <span className={`rounded-full text-base px-2.5 py-0.5 ${row.status_transaksi === 'SUCCESSFUL' ? 'bg-green-700 text-green-200' : 'bg-yellow-700 text-yellow-200'}`}>{row.status_transaksi}</span>,
+      ignoreRowClick: true,
+    },
+    {
+      name: 'Total Pembayaran',
+      sortable: true,
+      selector: (row: { total_pembayaran: any }) => new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(row.total_pembayaran)
+    },
+    {
+      name: 'Tanggal Transaksi',
+      sortable: true,
+      selector: (row: { created_at: any }) => moment(row.created_at).format('DD MMMM YYYY')
+    }
+  ], [])
 
   return (
     <div className='flex flex-1'>
       <Navbar />
       <div className="h-screen w-screen p-5">
         <h1 className='font-bold text-4xl text-hacienda-900'>Laporan Transaksi</h1>
-        {/* <DataTable title='Transaksi' /> */}
+        {isLoading && <LoaderComponent />}
+        {
+          data &&
+          <DataTable
+            data={data}
+            columns={columns}
+            pagination
+          />
+        }
       </div>
     </div>
   )
