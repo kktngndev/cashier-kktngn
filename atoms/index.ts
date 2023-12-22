6e8da40c918cf081258258f295ecd8c4e48237e5
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-22 11:17:27 +0800

    refactor: merge all atoms into index

--- atoms/index.ts
+++ atoms/index.ts
@@ -9,3 +9,13 @@ export const cartTotalAtom = atom({
   key: 'cartTotalAtom',
   default: 0
 })
+
+export const checkedCategoryAtom = atom({
+  key: 'checkedCategoryAtom',
+  default: 'Post Modernism'
+})
+
+export const paymentAtom = atom({
+  key: 'paymentAtom',
+  default: 0
+}) 
