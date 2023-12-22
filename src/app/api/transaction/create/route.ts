6d37571c23414b6ce291c0343864075bf7adf4b8
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-22 11:20:36 +0800

    feat(transaksi): add params for cash method

--- src/app/api/transaction/create/route.ts
+++ src/app/api/transaction/create/route.ts
@@ -13,13 +13,14 @@ export async function POST(req: NextRequest) {
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     });
 
-    const { id, status_transaksi, metode_pembayaran, total_pembayaran } = await req.json();
+    const { id, status_transaksi, metode_pembayaran, total_pembayaran, daftar_transaksi } = await req.json();
 
     const { error, status } = await supabase.from('transaksi').insert({
       id_transaksi: id,
       status_transaksi: status_transaksi,
       metode_pembayaran: metode_pembayaran,
       total_pembayaran: total_pembayaran,
+      daftar_transaksi: daftar_transaksi ?? daftar_transaksi
     })
 
     if (error) {
