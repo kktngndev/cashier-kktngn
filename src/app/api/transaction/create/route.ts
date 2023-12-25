5b051dcc018eddd9ae7f45d64b07897681f2d4a3
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-25 23:55:45 +0800

    fix(api): add status after inserting data

--- src/app/api/transaction/create/route.ts
+++ src/app/api/transaction/create/route.ts
@@ -14,19 +14,19 @@ export async function POST(req: NextRequest) {
 
   const { id, status_transaksi, metode_pembayaran, total_pembayaran, daftar_transaksi } = await req.json();
 
-  const { error, status } = await supabase.from('transaksi').insert({
+  const { error, statusText, status } = await supabase.from('transaksi').upsert({
     id_transaksi: id,
     status_transaksi: status_transaksi,
     metode_pembayaran: metode_pembayaran,
     total_pembayaran: total_pembayaran,
     daftar_transaksi: daftar_transaksi ?? daftar_transaksi
-  })
+  }).select();
 
   if (error) {
     return NextResponse.error();
   }
 
-  if (status) {
-    return NextResponse.json({ message: 'Success' });
+  if (statusText === 'Created') {
+    return NextResponse.json({ message: 'Success' }, { status } );
   }
 }
