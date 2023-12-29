9da077072cc3281499d7c837d9085eb0b4662ded
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-29 15:28:26 +0800

    chore: change supabase client component

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,5 +1,4 @@
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '../../types/database'
+import { createClient } from '@supabase/supabase-js'
+import { Database } from 'types/database'
 
-export const supabase = createClientComponentClient<Database>({ supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL, supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY})
-
+export const supabase = createClient<Database>(`${process.env.NEXT_PUBLIC_SUPABASE_URL}`, `${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`)
