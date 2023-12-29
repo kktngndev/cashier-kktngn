bf06d8d24ca8c409cff964fb57811566c8c5c884
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-29 15:27:40 +0800

    fix(api): set return data by date of month

--- src/app/api/report/getLastSevenDaysOrder/route.ts
+++ src/app/api/report/getLastSevenDaysOrder/route.ts
@@ -7,30 +7,26 @@ export async function GET() {
   const lastSevenDays = moment().subtract(7, 'days');
   const currentDay = moment();
 
-  const dayOrder = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
-
   const dateRange: string[] = []
   let currDateCopy = currentDay.clone();
 
   while (currDateCopy.isSameOrAfter(lastSevenDays, 'day')) {
-    dateRange.push(currDateCopy.format('dddd'));
+    dateRange.push(currDateCopy.format('YYYY-MM-DD'));
     currDateCopy = currDateCopy.subtract(1, 'day');
   }
-
-  const sortedDayNames = dateRange.sort((a, b) => dayOrder.indexOf(a) - dayOrder.indexOf(b));
 
   const { data, error } = await supabase.from('transaksi').select('created_at').gte('created_at', lastSevenDays.format('YYYY-MM-DD'));
 
   if (data) {
-    const totalOrdersPerDay: Record<string, number> = sortedDayNames.reduce((acc: any, dayName) => {
-      const ordersOnDay = data?.filter(order => moment(order?.created_at).format('dddd') === dayName) ?? [];
-      acc[dayName] = ordersOnDay.length;
+    const totalOrdersPerDay: Record<string, number> = dateRange.reduce((acc: any, date) => {
+      const ordersOnDay = data?.filter(order => moment(order?.created_at).format('YYYY-MM-DD') === date) ?? [];
+      acc[date] = ordersOnDay.length;
       return acc;
     }, {});
 
     // Convert the data to Chart.js format
     const chartData = Object.values(totalOrdersPerDay);
-    const chartLabels = sortedDayNames;
+    const chartLabels = dateRange.map(date => moment(date).format('DD/MM'));
 
     return NextResponse.json({ data: chartData, labels: chartLabels });
   }
