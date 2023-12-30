8e59f5a0589ba53314d25c99fffd51ed0706af2b
Muhammad Arief <64636917+itsmerifz@users.noreply.github.com>
2023-12-30 16:09:58 +0800

    fix(api): change supabase client import

--- src/app/api/transaction/create/route.ts
+++ src/app/api/transaction/create/route.ts
@@ -1,17 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { supabase } from "@/utils";
 
 export async function POST(req: NextRequest) {
-  const cookieStore = cookies();
-
-  const supabase = createRouteHandlerClient({
-    cookies: () => cookieStore
-  }, {
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  });
-
   const { id, status_transaksi, metode_pembayaran, total_pembayaran, daftar_transaksi } = await req.json();
 
   const { error, statusText, status } = await supabase.from('transaksi').upsert({
--- src/app/api/transaction/update/route.ts
+++ src/app/api/transaction/update/route.ts
@@ -1,17 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { supabase } from "@/utils";
 
 
 export async function PUT(req: NextRequest) {
-  const cookieStore = cookies();
-
-  const supabase = createRouteHandlerClient({
-    cookies: () => cookieStore
-  }, {
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  });
 
   const { id, daftar_transaksi } = await req.json();
 
